d8438ead61acac27f4cced0dc736605ceaa03ef3
Matthew Haneburger <teetertotterotter@gmail.com>
2016-04-19 13:37:29 -0400

    Update WebGraph.java

--- src/WebGraph.java
+++ src/WebGraph.java
@@ -16,6 +16,16 @@ private final static IntWritable one = new IntWritable(1);
 private Text word = new Text();
 
 
+ public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+	       String line = value.toString();
+	       StringTokenizer tokenizer = new StringTokenizer(line);
+	       while (tokenizer.hasMoreTokens()) {
+	         word.set(tokenizer.nextToken());
+	         output.collect(word, one);
+	       }
+	     }
+	   }
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
