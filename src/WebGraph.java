1aa6901e768a604f93e0da5e02294b50d403f595
Montana Wong <montanawong@gmail.com>
2016-04-19 13:38:49 -0400

    added example code 2nd half

--- src/WebGraph.java
+++ src/WebGraph.java
@@ -1,11 +1,40 @@
 
 public class WebGraph {
 
+	
+	
+	
+	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable>{
+		public void reduce (Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException{
+			int sum = 0;
+			
+			while(values.hasNext()){
+				sum += values.next().get();
+			}
+			output.collect(key, new IntWritable(sum));
+		}
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JobConf conf = new JobConf(WordCount.class);
+		conf.setJobName("wordcount");
+		
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+		
+		conf.setMapperClass(Map.class);
+		conf.setCombinerClass(Reduce.class);
+		conf.setReducerClass(Reduce.class);
+		
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+		
+		FileInputFormat.setInputPaths(conf, newPath(args[0]));
+		FileInputFormat.setOutputPaths(conf, newPath(args[1]));
+		
+		JobClient.runJob(conf);
 
 	}
 	
